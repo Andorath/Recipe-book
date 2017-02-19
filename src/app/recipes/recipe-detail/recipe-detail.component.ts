33b1c4b7ff20b01b0e25a89be49afc5cb22f9e1f
Marco Salafia <marco@MBP-di-Marco.homenet.telecomitalia.it>
2017-02-19 18:23:10 +0100

    Implementing routing and child routing

---
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -0,0 +1,53 @@
+import {Component, OnInit, OnDestroy} from '@angular/core';
+import {Recipe} from "../recipe";
+import {ShoppingListService} from "../../shopping-list/shopping-list.service";
+import {Router, ActivatedRoute} from "@angular/router";
+import {Subscription} from "rxjs";
+import {RecipeService} from "../recipe.service";
+
+@Component({
+  selector: 'rb-recipe-detail',
+  templateUrl: './recipe-detail.component.html',
+})
+export class RecipeDetailComponent implements OnInit, OnDestroy {
+  selectedRecipe: Recipe;
+  private recipeIndex: number;
+
+  /*Questa property è importante per conservare il salvataggio
+  * delle variazioni (subscriptions) sui parametri passati tramite Routing.
+  * Se non ci si preoccupa di fare unsubscribe nell onDestroy() si genererà un
+  * memory leak */
+  private subscription: Subscription;
+
+  constructor(private sls: ShoppingListService,
+              private router: Router,
+              private route: ActivatedRoute,
+              private recipeService: RecipeService) { }
+
+  ngOnInit() {
+    this.subscription = this.route.params.subscribe(
+      (params: any) => {
+        this.recipeIndex = params['id'];
+        this.selectedRecipe = this.recipeService.getRecipe(this.recipeIndex);
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  onAddToShoppingList() {
+    this.sls.addItems(this.selectedRecipe.ingredients);
+  }
+
+  onEdit() {
+    this.router.navigate(['/recipes', this.recipeIndex, 'edit'])
+  }
+
+  onDelete() {
+    this.recipeService.deleteRecipe(this.selectedRecipe);
+    this.router.navigate(['/recipes'])
+  }
+
+}
