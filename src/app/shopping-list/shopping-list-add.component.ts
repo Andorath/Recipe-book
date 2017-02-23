8466efb109499238bd3407aa160a7ad5985fd5ae
Marco Salafia <marco.salafia@gmail.com>
2017-02-23 22:02:28 +0100

    Lesson video 134

--- src/app/shopping-list/shopping-list-add.component.ts
+++ src/app/shopping-list/shopping-list-add.component.ts
@@ -1,14 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnChanges, Output, EventEmitter} from '@angular/core';
+import {Ingredient} from "../recipes/ingredient";
+import {ShoppingListService} from "./shopping-list.service";
 
 @Component({
   selector: 'rb-shopping-list-add',
   templateUrl: './shopping-list-add.component.html',
 })
-export class ShoppingListAddComponent implements OnInit {
+export class ShoppingListAddComponent implements OnChanges {
+  @Input() item: Ingredient;
+  @Output() cleared = new EventEmitter();
+  isAdd = true;
 
-  constructor() { }
+  constructor(private sls: ShoppingListService) { }
 
-  ngOnInit() {
+  //is called when any data-bound property of a directive changes
+  ngOnChanges(changes) {
+    if (changes.item.currentValue === null) {
+      this.isAdd = true;
+      this.item = {name: null, amount: null};
+    }else {
+      this.isAdd = false;
+    }
+  }
+
+  /*Invece di definire un nuovo Javascript Object ha preferito usare Ingredient
+  * in quanto è formato alla stessa maniera del form (un campo name e un campo amount)*/
+  onSubmit(ingredient: Ingredient){
+    const newIngredient = new Ingredient(ingredient.name, ingredient.amount);
+    if (!this.isAdd) {
+      this.sls.editItem(this.item, newIngredient);
+      this.onClear()
+    } else {
+      this.item = newIngredient;
+      this.sls.addItem(this.item);
+    }
+  }
+
+  onDelete() {
+    this.sls.deleteItem(this.item);
+    this.onClear()
+  }
+  onClear() {
+    this.isAdd = true;
+    this.cleared.emit(null);
   }
 
 }
