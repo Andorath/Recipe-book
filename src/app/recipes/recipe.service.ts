bbc72039c673dc2cb4aa58731d80bd74b619c93a
Marco Salafia <marco.salafia@gmail.com>
2017-02-26 01:38:09 +0100

    Lesson video 164

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,9 +1,14 @@
-import { Injectable } from '@angular/core';
+import {Injectable, EventEmitter} from '@angular/core';
+import {Headers, Http, Response} from "@angular/http";
+import 'rxjs/Rx';
+
 import {Recipe} from "./recipe";
 import {Ingredient} from "./ingredient";
 
 @Injectable()
 export class RecipeService {
+  recipesChanged = new EventEmitter<Recipe[]>();
+
   private recipes: Recipe[] = [
     new Recipe('Schnitzel', 'Very tasty', 'http://www.daringgourmet.com/wp-content/uploads/2014/03/Schnitzel-7_edited.jpg', [
       new Ingredient('French Fries', 2),
@@ -12,7 +17,7 @@ export class RecipeService {
     new Recipe('Summer Salad', 'Okayish', 'http://ohmyveggies.com/wp-content/uploads/2013/06/the_perfect_summer_salad.jpg', [])
   ]
 
-  constructor() { }
+  constructor(private http: Http) { }
 
   getRecipes() {
     return this.recipes;
@@ -32,6 +37,27 @@ export class RecipeService {
 
   editRecipe(oldRecipe: Recipe, newRecipe: Recipe) {
     this.recipes[this.recipes.indexOf(oldRecipe)] = newRecipe;
+  }
+
+  storeData() {
+    const body = JSON.stringify(this.recipes);
+    const headers = new Headers({
+      'Content-type' : 'application/json'
+    });
+    // like this https://recipebook-#####.firebaseio.com/recipes.json
+    return this.http.put('##############FIREBASE DB URL HERE#####################', body, {headers: headers});
+  }
+
+  fetchData() {
+    // like this https://recipebook-#####.firebaseio.com/recipes.json
+    return this.http.get('##############FIREBASE DB URL HERE#####################')
+      .map((response: Response) => response.json())
+      .subscribe(
+        (data: any) => {
+          this.recipes = data;
+          this.recipesChanged.emit(this.recipes);
+        }
+      );
   }
 
 }
